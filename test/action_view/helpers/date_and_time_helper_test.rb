9f076c3cea6c5dc3c7cdc2e849f2a88ef2ade7f6
Akira Shirase <slava.kravchenko@gmail.com>
2013-03-07 11:49:00 +0300

    DateAndTimeHelper test

---
+++ test/action_view/helpers/date_and_time_helper_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+describe ActionView::Helpers::DateAndTimeHelper do
+  include ActionView::Helpers::DateAndTimeHelper
+
+  describe "date_and_time_field" do
+    subject { date_and_time_field(:object, :created_at) }
+
+    it "must produce proper markup" do
+      subject.must_equal "<input id=\"object_created_at_1\" name=\"object[created_at(1)]\" type=\"date\"/><input id=\"object_created_at_2\" name=\"object[created_at(2)]\" type=\"time\"/>"
+    end
+  end
+end
