e0dfffe594920e65a47c32f404464c0a343c347f
Akira Shirase <slava.kravchenko@gmail.com>
2013-03-07 15:28:01 +0300

    shaping the form helpers

--- lib/action_view/helpers/date_and_time_helper.rb
+++ lib/action_view/helpers/date_and_time_helper.rb
@@ -4,38 +4,57 @@ module ActionView
       DEFAULT_PREFIX = 'date'.freeze
       POSITION = { :date => 1, :time => 2 }.freeze
 
-      def date_and_time_field(object_name, method, options = {})
-        InstanceTag.new(object_name, method, self, options.delete(:object)).to_date_and_time_field_tag(options)
+      def date_and_time_field(object_name, method, options = {}, html_options = {})
+        InstanceTag.new(object_name, method, self, options.delete(:object)).to_date_and_time_field_tag(options, html_options)
       end
     end
 
     class InstanceTag
-      def to_date_and_time_field_tag(options = {})
-        # TODO: generate fields with name = "method(1)" and "method(2)"
-        tag("input", options)
+      def to_date_and_time_field_tag(options = {}, html_options = {})
+        options = options.stringify_keys
+        html_options = html_options.stringify_keys
+        html_options["value"] = html_options.fetch("value"){ value_before_type_cast(object) }
+        html_options["value"] &&= ERB::Util.html_escape(html_options["value"])
+        { date: value_to_date(html_options["value"]),
+          time: value_to_time(html_options["value"])
+        }.inject("") do |output, (field_type, value)|
+          html_options.merge!("id" => input_id_from_type(field_type, options),
+                              "name" => input_name_from_type(field_type, options),
+                              "type" => field_type,
+                              "value" => value)
+          output << tag("input", html_options)
+        end
       end
 
       private
 
-      # Returns the name attribute for the input tag.
-      #  => post[written_on(1i)]
-      def input_name_from_type(type)
-        prefix = @options[:prefix] || ActionView::Helpers::DateAndTimeHelper::DEFAULT_PREFIX
-        prefix += "[#{@options[:index]}]" if @options.has_key?(:index)
+      def value_to_date(value)
+        value && value.to_date.to_s
+      end
 
-        field_name = @options[:field_name] || type
-        if @options[:include_position]
+      def value_to_time(value)
+        value && value.strftime("%H:%M")
+      end
+
+      # Returns the name attribute for the input tag.
+      #  => post[written_on(1)]
+      def input_name_from_type(type, options = {})
+        prefix = options["prefix"] || ActionView::Helpers::DateAndTimeHelper::DEFAULT_PREFIX
+        prefix += "[#{options['index']}]" if options.has_key?("index")
+
+        field_name = options["field_name"] || type
+        if options["include_position"]
           field_name += "(#{ActionView::Helpers::DateAndTimeHelper::POSITION[type]})"
         end
 
-        @options[:discard_type] ? prefix : "#{prefix}[#{field_name}]"
+        options["discard_type"] ? prefix : "#{prefix}[#{field_name}]"
       end
 
       # Returns the id attribute for the input tag.
-      #  => "post_written_on_1i"
-      def input_id_from_type(type)
+      #  => "post_written_on_1"
+      def input_id_from_type(type, options = {})
         id = input_name_from_type(type).gsub(/([\[\(])|(\]\[)/, '_').gsub(/[\]\)]/, '')
-        id = @options[:namespace] + '_' + id if @options[:namespace]
+        id = options["namespace"] + '_' + id if options["namespace"]
 
         id
       end
