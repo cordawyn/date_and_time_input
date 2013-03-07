9a714ca2030fda76013072d83beab415384b4ca9
Akira Shirase <slava.kravchenko@gmail.com>
2013-03-07 11:48:42 +0300

    copied some helper methods from the original ActionView

--- lib/action_view/helpers/date_and_time_helper.rb
+++ lib/action_view/helpers/date_and_time_helper.rb
@@ -1,6 +1,9 @@
 module ActionView
   module Helpers
     module DateAndTimeHelper
+      DEFAULT_PREFIX = 'date'.freeze
+      POSITION = { :date => 1, :time => 2 }.freeze
+
       def date_and_time_field(object_name, method, options = {})
         InstanceTag.new(object_name, method, self, options.delete(:object)).to_date_and_time_field_tag(options)
       end
@@ -10,6 +13,31 @@ module ActionView
       def to_date_and_time_field_tag(options = {})
         # TODO: generate fields with name = "method(1)" and "method(2)"
         tag("input", options)
+      end
+
+      private
+
+      # Returns the name attribute for the input tag.
+      #  => post[written_on(1i)]
+      def input_name_from_type(type)
+        prefix = @options[:prefix] || ActionView::Helpers::DateAndTimeHelper::DEFAULT_PREFIX
+        prefix += "[#{@options[:index]}]" if @options.has_key?(:index)
+
+        field_name = @options[:field_name] || type
+        if @options[:include_position]
+          field_name += "(#{ActionView::Helpers::DateAndTimeHelper::POSITION[type]})"
+        end
+
+        @options[:discard_type] ? prefix : "#{prefix}[#{field_name}]"
+      end
+
+      # Returns the id attribute for the input tag.
+      #  => "post_written_on_1i"
+      def input_id_from_type(type)
+        id = input_name_from_type(type).gsub(/([\[\(])|(\]\[)/, '_').gsub(/[\]\)]/, '')
+        id = @options[:namespace] + '_' + id if @options[:namespace]
+
+        id
       end
     end
 
