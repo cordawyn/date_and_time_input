105c1f82f97b84c302c08f9923be9fa151e31543
Akira Shirase <slava.kravchenko@gmail.com>
2013-03-07 15:53:26 +0300

    load submodules only when Rails' modules are available

--- lib/date_and_time_input.rb
+++ lib/date_and_time_input.rb
@@ -1,2 +1,8 @@
-require "action_view/helpers/date_and_time_helper"
-require "active_record/datetime_multiparameter_attribute"
+if defined?(::ActionView)
+  require "action_view/helpers/date_and_time_helper"
+end
+
+if defined?(::ActiveRecord)
+  require "active_record/datetime_multiparameter_attribute"
+  ActiveRecord::Base.send :include, DatetimeMultiparameterAttribute
+end
