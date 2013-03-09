7996c51dd9df4bbfd280b1028c0d006d43ba0d78
Akira Shirase <slava.kravchenko@gmail.com>
2013-03-09 09:52:59 +0300

    moved the overridden code into #read_time_parameter_value
    fixed time assignment

--- lib/active_record/datetime_multiparameter_attribute.rb
+++ lib/active_record/datetime_multiparameter_attribute.rb
@@ -3,21 +3,12 @@ module ActiveRecord
   module DatetimeMultiparameterAttribute
     private
 
-    def read_value_from_parameter(name, values_hash_from_param)
-      klass = (self.class.reflect_on_aggregation(name.to_sym) || column_for_attribute(name)).klass
-      if (klass == Time || klass == Datetime) && values_hash_from_param.size == 2
+    def read_time_parameter_value(name, values_hash_from_param)
+      if values_hash_from_param.size == 2
         t = Time.zone.parse("#{values_hash_from_param[1]} #{values_hash_from_param[2]}")
         if t
-          values = {
-            1 => t.year,
-            2 => t.month,
-            3 => t.day,
-            4 => t.hour,
-            5 => t.min,
-            6 => t.sec,
-            7 => t.usec
-          }
-          read_other_parameter_value(klass, name, values)
+          values = [t.year, t.month, t.day, t.hour, t.min, t.sec, t.usec]
+          instantiate_time_object(name, values)
         else
           nil
         end
